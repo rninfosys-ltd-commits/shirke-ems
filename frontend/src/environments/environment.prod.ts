387cd71f3f0463d7cc0c7603ed57122aedd4e10f
rninfosys-ltd-commits <rninfosysltd@gmail.com>
2026-07-02 09:34:41 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://independent-determination-production.up.railway.app'
-  apiUrl: 'http://103.168.19.63:8080/shirke-erp'
+  apiUrl: 'https://independent-determination-production.up.railway.app'
+  // apiUrl: 'http://103.168.19.63:8080/shirke-erp'
 };
