e9509b2e0c834c545bb4561a1509001327fffbe7
Rn Infosys <rninfosysltd@gmail.com>
2026-07-13 16:07:37 +0530

    changed the api url

--- frontend/src/app/config/config.ts
+++ frontend/src/app/config/config.ts
@@ -1,4 +1,4 @@
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 export const APP_CONFIG = {
     BASE_URL: environment.apiUrl,
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://supportive-benevolence-production-952e.up.railway.app'
+  apiUrl: 'https://splendid-celebration-production-8295.up.railway.app'
   // apiUrl: 'http://103.168.19.63:8080/shirke-erp'
 };
